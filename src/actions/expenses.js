a57de0dd4193fcbd44810e6750fc748f0090f9b0
hailu <hailu@cm-84.210.144.115.getinternet.no>
2020-07-14 15:23:44 +0200

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -43,3 +43,30 @@ export const editExpense = (id, updates) => (
         id,
         updates
     });
+
+// SET_EXPENSES
+
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+})
+
+// startSetExpenses
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+
+            snapshot.forEach((childSnapshot) => {
+                const expense = {
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                };
+                
+                expenses.push(expense);
+            })
+            dispatch(setExpenses(expenses))
+        })
+    }
+} 
